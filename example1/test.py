b3d63bf2a4c094b2f761e8b53738a74900711808
jamiejamiebobamie <jmccrory@vt.edu>
2020-03-21 22:13:10 -0700

    having similiar issues with convergance. the models IoU accuracy isnt getting better. inspecting the Validation function for errors. also looking at input files.

--- example1/test.py
+++ example1/test.py
@@ -5,7 +5,7 @@ import numpy as np
 from train import create_model, IMAGE_SIZE
 from keras.applications.mobilenetv2 import preprocess_input
 
-WEIGHTS_FILE = "model-0.29.h5"
+WEIGHTS_FILE = "model-0.52.h5"
 IMAGES = "images/*jpg"
 
 def main():
@@ -19,19 +19,15 @@ def main():
         image = cv2.resize(unscaled, (IMAGE_SIZE, IMAGE_SIZE))
         feat_scaled = preprocess_input(np.array(image, dtype=np.float32))
 
-        region, class_id = model.predict(x=np.array([image]))
-        region = region[0]
+        region = model.predict(x=np.array([feat_scaled]))[0]
 
         x0 = int(region[0] * image_width / IMAGE_SIZE)
-        y0 = int(region[1]  * image_height / IMAGE_SIZE)
+        y0 = int(region[1] * image_height / IMAGE_SIZE)
 
         x1 = int((region[0] + region[2]) * image_width / IMAGE_SIZE)
         y1 = int((region[1] + region[3]) * image_height / IMAGE_SIZE)
 
-        class_id = np.argmax(class_id, axis=1)
-
         cv2.rectangle(unscaled, (x0, y0), (x1, y1), (0, 0, 255), 1)
-        cv2.putText(unscaled, "class: {}".format(class_id[0]), (x0, y0), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
         cv2.imshow("image", unscaled)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -3,15 +3,17 @@ import math
 
 from PIL import Image
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, Callback
-from tensorflow.keras.layers import Conv2D, Reshape
+from tensorflow.keras.layers import Conv2D, Reshape, Dense, GlobalAveragePooling2D
 from tensorflow.keras.utils import Sequence
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.backend import epsilon
 
 # 0.35, 0.5, 0.75, 1.0
-ALPHA = 1.0
+ALPHA = 0.35
 
 # 96, 128, 160, 192, 224
 IMAGE_SIZE = 96
@@ -26,19 +28,19 @@ THREADS = 1
 TRAIN_CSV = "train.csv"
 VALIDATION_CSV = "validation.csv"
 
+CLASSES = 2
+
+class_names = {"Car":0,"Pedestrian":1}
+image_width = 1242
+image_height = 375
 
 class DataGenerator(Sequence):
 
     def __init__(self, csv_file):
         self.paths = []
 
-        # the width and height of the majority of the images in the dataset
-        self.dimensions = {'width':1242, 'height':375}
-        image_width = self.dimensions['width']
-        image_height = self.dimensions['height']
-
         with open(csv_file, "r") as file:
-            self.coords = np.zeros((sum(1 for line in file), 4))
+            self.coords = np.zeros((sum(1 for line in file), 4 + CLASSES))
             file.seek(0)
 
             reader = csv.reader(file, delimiter=",")
@@ -47,11 +49,13 @@ class DataGenerator(Sequence):
                     r = float(r)
                     row[i+1] = int(r)
 
-                path, x0, y0, x1, y1, _ = row
+                path, x0, y0, x1, y1, class_name = row
                 self.coords[index, 0] = x0 * IMAGE_SIZE / image_width
                 self.coords[index, 1] = y0 * IMAGE_SIZE / image_height
                 self.coords[index, 2] = (x1 - x0) * IMAGE_SIZE / image_width
                 self.coords[index, 3] = (y1 - y0) * IMAGE_SIZE / image_height
+                self.coords[index, min(4 + class_names[class_name],
+                                                self.coords.shape[1]-1)] = 1
 
                 self.paths.append(path)
 
@@ -62,7 +66,8 @@ class DataGenerator(Sequence):
         batch_paths = self.paths[idx * BATCH_SIZE:(idx + 1) * BATCH_SIZE]
         batch_coords = self.coords[idx * BATCH_SIZE:(idx + 1) * BATCH_SIZE]
 
-        batch_images = np.zeros((len(batch_paths), IMAGE_SIZE, IMAGE_SIZE, 3), dtype=np.float32)
+        batch_images = np.zeros((len(batch_paths), IMAGE_SIZE, IMAGE_SIZE, 3),
+                                                            dtype=np.float32)
         for i, f in enumerate(batch_paths):
             img = Image.open(f)
             img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
@@ -71,7 +76,7 @@ class DataGenerator(Sequence):
             batch_images[i] = preprocess_input(np.array(img, dtype=np.float32))
             img.close()
 
-        return batch_images, batch_coords
+        return batch_images, [batch_coords[...,:4], batch_coords[...,4:]]
 
 class Validation(Callback):
     def __init__(self, generator):
@@ -79,19 +84,30 @@ class Validation(Callback):
 
     def on_epoch_end(self, epoch, logs):
         mse = 0
+        accuracy = 0
+
         intersections = 0
         unions = 0
 
         for i in range(len(self.generator)):
-            batch_images, gt = self.generator[i]
-            pred = self.model.predict_on_batch(batch_images)
+            batch_images, (gt, class_id) = self.generator[i]
+            print(gt)
+            break
+            pred, pred_class = self.model.predict_on_batch(batch_images)
+            # euclidean distance between the ground truth coords and the pred
             mse += np.linalg.norm(gt - pred, ord='fro') / pred.shape[0]
+
+            pred_class = np.argmax(pred_class, axis=1)
+            accuracy += np.sum(np.argmax(class_id, axis=1) == pred_class)
 
             pred = np.maximum(pred, 0)
 
-            diff_width = np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2]) - np.maximum(gt[:,0], pred[:,0])
-            diff_height = np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3]) - np.maximum(gt[:,1], pred[:,1])
-            intersection = np.maximum(diff_width, 0) * np.maximum(diff_height, 0)
+            diff_width = (np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2])
+                                            - np.maximum(gt[:,0], pred[:,0]))
+            diff_height = (np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3])
+                                            - np.maximum(gt[:,1], pred[:,1]))
+            intersection = (np.maximum(diff_width, 0)
+                                                * np.maximum(diff_height, 0))
 
             area_gt = gt[:,2] * gt[:,3]
             area_pred = pred[:,2] * pred[:,3]
@@ -106,45 +122,82 @@ class Validation(Callback):
         mse = np.round(mse, 4)
         logs["val_mse"] = mse
 
-        print(" - val_iou: {} - val_mse: {}".format(iou, mse))
+        accuracy = np.round(accuracy / len(self.generator.coords), 4)
+        logs["val_acc"] = accuracy
+
+        print(" - val_iou: {} - val_mse: {} - val_acc: {}".format(iou,
+                                                                mse, accuracy))
+
 
 def create_model(trainable=False):
-    model = MobileNetV2(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), include_top=False, alpha=ALPHA)
+    model = MobileNetV2(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3),
+                                                include_top=False, alpha=ALPHA)
 
     # to freeze layers
     for layer in model.layers:
         layer.trainable = trainable
 
-    x = model.layers[-1].output
-    x = Conv2D(4, kernel_size=3, name="coords")(x)
-    x = Reshape((4,))(x)
+    out = model.layers[-1].output
 
-    return Model(inputs=model.input, outputs=x)
+    x = Conv2D(4, kernel_size=3)(out)
+    x = Reshape((4,), name="coords")(x)
+
+    y = GlobalAveragePooling2D()(out)
+    y = Dense(CLASSES, name="classes", activation="softmax")(y)
+
+    return Model(inputs=model.input, outputs=[x, y])
+
+
+def log_mse(y_true, y_pred):
+    return tf.reduce_mean(tf.math.log1p(tf.math.squared_difference(y_pred,
+                                                            y_true)), axis=-1)
+
+def focal_loss(alpha=0.9, gamma=2):
+  def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+    weight_a = alpha * (1 - y_pred) ** gamma * targets
+    weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+
+    return ((tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits))
+                                    * (weight_a + weight_b) + logits * weight_b)
+
+  def loss(y_true, y_pred):
+    y_pred = tf.clip_by_value(y_pred, epsilon(), 1 - epsilon())
+    logits = tf.math.log(y_pred / (1 - y_pred))
+
+    loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha,
+                                                    gamma=gamma, y_pred=y_pred)
+
+    return tf.reduce_mean(loss)
+
+  return loss
 
 def main():
     model = create_model()
-    model.summary()
 
     train_datagen = DataGenerator(TRAIN_CSV)
     validation_datagen = Validation(generator=DataGenerator(VALIDATION_CSV))
 
-    model.compile(loss="mean_squared_error", optimizer="adam", metrics=[])
-
-    checkpoint = ModelCheckpoint("model-{val_iou:.2f}.h5", monitor="val_iou", verbose=1, save_best_only=True,
+    optimizer = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999,
+                                    epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss={"coords" : log_mse, "classes" : focal_loss()},
+    loss_weights={"coords" : 1, "classes" : 1}, optimizer=optimizer, metrics=[])
+    checkpoint = ModelCheckpoint("model-{val_iou:.2f}.h5", monitor="val_iou",
+                                verbose=1, save_best_only=True,
                                  save_weights_only=True, mode="max")
     stop = EarlyStopping(monitor="val_iou", patience=PATIENCE, mode="max")
-    reduce_lr = ReduceLROnPlateau(monitor="val_iou", factor=0.2, patience=10, min_lr=1e-7, verbose=1, mode="max")
+    reduce_lr = ReduceLROnPlateau(monitor="val_iou", factor=0.2, patience=10,
+                                        min_lr=epsilon(), verbose=1, mode="max")
+
+    model.summary()
 
     model.fit_generator(generator=train_datagen,
                         epochs=EPOCHS,
-                        callbacks=[validation_datagen, checkpoint, reduce_lr, stop],
+                        callbacks=[validation_datagen, checkpoint,
+                                                    reduce_lr, stop],
                         workers=THREADS,
                         use_multiprocessing=MULTI_PROCESSING,
                         shuffle=True,
                         verbose=1)
-
-    model.save_weights("model.h5")
-
 
 
 if __name__ == "__main__":
